323f3fb9ae57a90825a910430e43c2a6dae952fb
BellaAguirre <yunoshe1@gmail.com>
2018-06-13 23:33:59 -0500

    insertando datos de la data a select y tabla

--- src/data.js
+++ src/data.js
@@ -1,17 +1,52 @@
-const xhr = new XMLHttpRequest();
+function getUser(){
+    const xhr = new XMLHttpRequest();
 xhr.open('GET',`../data/cohorts/lim-2018-03-pre-core-pw/users.json`);
 xhr.onload = addUser;
 xhr.onerror = handleError;
 xhr.send();
+}
 
-
+function getCohorts(){
+    const xhrCohorts = new XMLHttpRequest();
+    xhrCohorts.open('GET', `../data/cohorts.json`);
+    xhrCohorts.onload = addCohorts;
+    xhrCohorts.onerror = handleError;
+    xhrCohorts.send(); 
+}
  
 function handleError(){
     console.log('se ha presentado un error');
 }
-
+const li = document.getElementById('lima');
+const generacion = document.getElementById('generacion');
+const table = document.getElementById('container-user');
+generacion.addEventListener('click', function(e){
+    table.innerHTML = '';
+    getUser();
+});
 function addUser(){
-    const data = JSON.parse(xhr.responseText);
-    console.log(data[0]);
+  
+    const data = JSON.parse(this.responseText);
+    for (let i = 0; i < data.length; i++) {
+        let tr = document.createElement('tr');
+        tr.innerText += data[i].name;
+        table.appendChild(tr);
+    }
     
+}
+li.addEventListener('click', function(e){
+    generacion.innerHTML = '';
+    getCohorts();
+
+});
+function addCohorts(){
+    const dataCohorts = JSON.parse(this.responseText);
+    //console.log(dataCohorts[0]);
+    
+    for(var i=0;i<dataCohorts.length;i++){
+        let option = document.createElement('option');
+        option.setAttribute('value',dataCohorts[i].id)
+        option.innerText += dataCohorts[i].id ;
+        generacion.appendChild(option);
+    }
 }
