876c5b143faeed3ada6f4a97866ab5a97e6d8f09
BellaAguirre <yunoshe1@gmail.com>
2018-06-14 16:55:58 -0500

    mostrando lista de estudiantes de un cohorts

--- src/data.js
+++ src/data.js
@@ -9,6 +9,14 @@ function getCohorts(){
     xhrCohorts.onerror = handleError;
     xhrCohorts.send(); 
 }
+//conectando data user
+function getUser(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET',`../data/cohorts/lim-2018-03-pre-core-pw/users.json`);
+    xhr.onload = addUser;
+    xhr.onerror = handleError;
+    xhr.send();
+}
 function handleError(){
     console.log('se ha presentado un error');
 }
@@ -26,3 +34,19 @@ function addCohorts(){
         generacion.appendChild(option);
     }
 }
+function addUser(){
+    const data = JSON.parse(event.currentTarget.responseText);
+    for (let i = 0; i < data.length; i++) {
+        let tr = document.createElement('tr');
+        tr.innerText += data[i].name;
+        table.appendChild(tr);
+    }
+}
+generacion.addEventListener('change', function(e){
+    if(generacion.value === 'lim-2018-03-pre-core-pw'){
+    table.innerHTML = '';
+    getUser();
+    }else{
+        table.innerHTML = 'No hay datos para mostrar';
+    }
+});
