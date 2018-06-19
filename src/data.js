e0b708ead4b2e7fe6f79763bfcdf1142e39ebfb4
BellaAguirre <yunoshe1@gmail.com>
2018-06-18 23:37:27 -0500

    total ejercicios

--- src/data.js
+++ src/data.js
@@ -3,93 +3,116 @@ const li = document.getElementById('lima');
 const generacion = document.getElementById('generacion');
 const table = document.getElementById('container-user');
 
-function getUsers(){
-    const xhr = new XMLHttpRequest();
-        xhr.open('GET',`../data/cohorts/lim-2018-03-pre-core-pw/users.json`);
-        xhr.onload = function () {
-            const users = JSON.parse(event.currentTarget.responseText);
-            const xhrCohorts = new XMLHttpRequest();
-            xhrCohorts.open('GET', `../data/cohorts/lim-2018-03-pre-core-pw/progress.json`);
-            xhrCohorts.onload = function () {
-                const progress = JSON.parse(event.currentTarget.responseText);
-                //console.log('cohort',dataCohorts.id = 'lim-2018-03-pre-core-pw')
-                const computeUsersStats = () => {
-                    const dataUser = users.map(user => {
-                        //console.log(progress[user.id].intro.units)
-                        const percentProgress = () =>{
-                            /* if(progress[user.id].hasOwnProperty('intro')) {
-                                if(progress[user.id].intro.hasOwnProperty('percent')) {
-                                    return progress[user.id].intro.percent;
-                                } 
-                            }  */
-                            const percent =[];
-                            Object.keys(progress[user.id]).map( course =>{
-                                if (progress[user.id][course].hasOwnProperty('percent')) {
-                                     percent.push(progress[user.id][course].percent);
-                                }
-                            });
-                            return parseInt(percent.toString());
-                        }
-                        const exercisesProgress = () => {
-                            const exercise = [];
-                            Object.keys(progress[user.id]).map(course => {
-                                Object.keys(progress[user.id][course].units).map(leccion => {
-                                    Object.keys(progress[user.id][course].units[leccion].parts).map(lectura => {
-                                                  if(progress[user.id][course].units[leccion].parts[lectura].hasOwnProperty('exercises')){
-                                             exercise.push(Object.values(progress[user.id][course].units[leccion].parts[lectura].exercises).length);
-
-                                            }
-                                              })  
-                                        })
-                            });
-                            return exercise;
-                            /* if(progress[user.id].hasOwnProperty('intro')){
-                              if(progress[user.id].intro.hasOwnProperty('units')) {
-                                if(progress[user.id].intro.units['02-variables-and-data-types'].parts['06-exercises'].hasOwnProperty('exercises')){
-                                    return Object.values(progress[user.id].intro.units['02-variables-and-data-types'].parts['06-exercises'].exercises).length
-                                }
-                              }
-                            } */
-                        }
-                        
-                       /*  const exercisesCompleted = () => {
-                            if(progress[user.id].hasOwnProperty('intro')){
-                              if(progress[user.id].intro.hasOwnProperty('units')) {
-                                if(progress[user.id].intro.units['02-variables-and-data-types'].parts['06-exercises'].hasOwnProperty('completed')){
-                                    return progress[user.id].intro.units['02-variables-and-data-types'].parts['06-exercises'].completed;
-                                }
-                              }
-                            }
-                        } */
-                      
-                           
-                        const obj = {
-                            percent: percentProgress(),
-                            exercises: {
-                                total: exercisesProgress(),/* 
-                                completed: exercisesCompleted(),
-                                percent: exercisesPercent(), */
-
-                            }
-                        };
-                            return obj;
-                    });
-                console.log(dataUser);
-                }
-                computeUsersStats()
+function getUsers() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `../data/cohorts/lim-2018-03-pre-core-pw/users.json`);
+  xhr.onload = function () {
+    const users = JSON.parse(event.currentTarget.responseText);
+    const xhrCohorts = new XMLHttpRequest();
+    xhrCohorts.open('GET', `../data/cohorts/lim-2018-03-pre-core-pw/progress.json`);
+    xhrCohorts.onload = function () {
+      const progress = JSON.parse(event.currentTarget.responseText);
+      //console.log('cohort',dataCohorts.id = 'lim-2018-03-pre-core-pw')
+      const computeUsersStats = () => {
+        const dataUser = users.map(user => {
+          //console.log(progress[user.id].intro.units)
+          const percentProgress = () => {
+            /* if(progress[user.id].hasOwnProperty('intro')) {
+                if(progress[user.id].intro.hasOwnProperty('percent')) {
+                    return progress[user.id].intro.percent;
+                } 
+            }  */
+            const percent = [];
+            Object.keys(progress[user.id]).map(course => {
+              if (progress[user.id][course].hasOwnProperty('percent')) {
+                percent.push(progress[user.id][course].percent);
+              }
+            });
+            if (percent[0] === undefined) {
+              return percent[0] = 0;
+            } else {
+              return percent[0];
             }
-            xhrCohorts.onerror = handleError;
-            xhrCohorts.send(); 
-        };
-        xhr.onerror = handleError;
-        xhr.send();
+          }
+          const exercisesProgress = () => {
+            const total = [];
+            Object.keys(progress[user.id]).map(course => {
+              Object.keys(progress[user.id][course].units).map(leccion => {
+                Object.keys(progress[user.id][course].units[leccion].parts).map(lectura => {
+                  if (progress[user.id][course].units[leccion].parts[lectura].hasOwnProperty('exercises')) {
+                    total.push(Object.values(progress[user.id][course].units[leccion].parts[lectura].exercises).length);
+                  }
+                })
+              })
+            });
+            if (total[0] === undefined) {
+              return total[0] = 0;
+            } else {
+              return total[0];
+            }
+
+            /* if(progress[user.id].hasOwnProperty('intro')){
+              if(progress[user.id].intro.hasOwnProperty('units')) {
+                if(progress[user.id].intro.units['02-variables-and-data-types'].parts['06-exercises'].hasOwnProperty('exercises')){
+                    return Object.values(progress[user.id].intro.units['02-variables-and-data-types'].parts['06-exercises'].exercises).length
+                }
+              }
+            } */
+          }
+
+          const exercisesCompleted = () => {
+            /*  if (progress[user.id].hasOwnProperty('intro')) {
+              if (progress[user.id].intro.hasOwnProperty('units')) {
+                if (progress[user.id].intro.units['02-variables-and-data-types'].parts['06-exercises'].exercises['01-coin-convert'].hasOwnProperty('completed')) {
+                  return progress[user.id].intro.units['02-variables-and-data-types'].parts['06-exercises'].exercises['01-coin-convert'].completed;
+                }
+              }
+            } */ 
+            const completed = [];
+            Object.keys(progress[user.id]).map(course => {
+              Object.keys(progress[user.id][course].units).map(leccion => {
+                Object.keys(progress[user.id][course].units[leccion].parts).map(lectura => {
+                  Object.keys(progress[user.id][course].units[leccion].parts[lectura].exercises).map(exercise=>{
+                    if(progress[user.id][course].units[leccion].parts[lectura].exercises[exercise].hasOwnProperty('completed')){
+                      completed.push(progress[user.id][course].units[leccion].parts[lectura].exercises[exercise].completed);
+                    }
+                  }
+
+                  )       
+                })
+              })
+            });
+            return completed;
+          }
+
+
+          const obj = {
+            percent: percentProgress(),
+            exercises: {
+              total: exercisesProgress(),
+              completed: exercisesCompleted(),
+              //percent: exercisesPercent(),
+
+            }
+          };
+          return obj;
+        });
+        console.log(dataUser);
+      }
+      computeUsersStats()
+    }
+    xhrCohorts.onerror = handleError;
+    xhrCohorts.send();
+  };
+  xhr.onerror = handleError;
+  xhr.send();
 } // 
-li.addEventListener('click', function(e){
-    generacion.innerHTML = '';
-    getUsers();
+li.addEventListener('click', function (e) {
+  generacion.innerHTML = '';
+  getUsers();
 });
-function handleError(){
-    console.log('se ha presentado un error');
+function handleError() {
+  console.log('se ha presentado un error');
 }
 /* obteniendo data cohorts
 function getCohorts(){
