9132a76e8b3be7f7ec6f1c6cc21fca00af83fc03
BellaAguirre <yunoshe1@gmail.com>
2018-07-02 02:06:40 -0500

    cambiando creacion de tabla por divs, llamando la funcion processCohortData

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 const sedeSelect = document.getElementById('sede');
-const generacion = document.getElementById('generacion');
-const tblBody = document.getElementById('container-user');
-const stringSearch = document.getElementById('search');
-const searchBtn = document.getElementById('btnsearch');
+const generationSelect = document.getElementById('generacion');
+const selectProgram = document.getElementById('programa');
 const selectOrderDirection = document.getElementById('orderDirection');
 const selectOrderBy = document.getElementById('orderBy');
+const stringSearch = document.getElementById('search');
 const ordenar = document.getElementById('ordenar')
-const selectProgram = document.getElementById('programa');
-
 var sectionContentStudents = document.getElementById("showing");
 
 let options = {
@@ -20,6 +17,40 @@ let options = {
   orderDirection: '',
   search: '',
 
+}
+function getUsers() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `../data/cohorts/lim-2018-03-pre-core-pw/users.json`);
+  xhr.onload = function () {
+    const usersData = JSON.parse(event.currentTarget.responseText);
+    const xhrCohorts = new XMLHttpRequest();
+    xhrCohorts.open('GET', `../data/cohorts/lim-2018-03-pre-core-pw/progress.json`);
+    xhrCohorts.onload = function () {
+      const progress = JSON.parse(event.currentTarget.responseText);
+      const users = [];
+      usersData.map(user => {
+        if (generationSelect.value === user.signupCohort) {
+          users.push(user);
+        }
+      })
+      addUser(users, progress)
+    }
+    xhrCohorts.onerror = handleError;
+    xhrCohorts.send();
+  };
+  xhr.onerror = handleError;
+  xhr.send();
+}
+function handleError() {
+  console.log('se ha presentado un error');
+}
+//obteniendo data cohorts
+function getCohorts(callback) {
+  const xhrCohorts = new XMLHttpRequest();
+  xhrCohorts.open('GET', `../data/cohorts.json`);
+  xhrCohorts.onload = callback;
+  xhrCohorts.onerror = handleError;
+  xhrCohorts.send();
 }
 function pasarDatos(users, progress, cohortSelect) {
   options.cohort = cohortSelect[0];
@@ -46,40 +77,6 @@ function pasarDatos(users, progress, cohortSelect) {
     dataTable(userfilter);
   })
 }
-function getUsers() {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', `../data/cohorts/lim-2018-03-pre-core-pw/users.json`);
-  xhr.onload = function () {
-    const usersData = JSON.parse(event.currentTarget.responseText);
-    const xhrCohorts = new XMLHttpRequest();
-    xhrCohorts.open('GET', `../data/cohorts/lim-2018-03-pre-core-pw/progress.json`);
-    xhrCohorts.onload = function () {
-      const progress = JSON.parse(event.currentTarget.responseText);
-      const users = [];
-      usersData.map(user => {
-        if (generacion.value === user.signupCohort) {
-          users.push(user);
-        }
-      })
-      addUser(users, progress)
-    }
-    xhrCohorts.onerror = handleError;
-    xhrCohorts.send();
-  };
-  xhr.onerror = handleError;
-  xhr.send();
-}
-function handleError() {
-  console.log('se ha presentado un error');
-}
-//obteniendo data cohorts
-function getCohorts(callback) {
-  const xhrCohorts = new XMLHttpRequest();
-  xhrCohorts.open('GET', `../data/cohorts.json`);
-  xhrCohorts.onload = callback;
-  xhrCohorts.onerror = handleError;
-  xhrCohorts.send();
-}
 function addUser(users, progress) {
   getCohorts(() => {
     const dataCohorts = JSON.parse(event.target.responseText);
@@ -91,7 +88,6 @@ function addUser(users, progress) {
     }
     pasarDatos(users, progress, cohortSelect);
   })
-
 }
 
 function dataTable(datos) {
@@ -103,11 +99,11 @@ function dataTable(datos) {
     const newDivName = document.createElement('div');
     const divPrincipal = document.createElement('div');
     //creando atributos a los div
-    newDivStudent.setAttribute('id','div-student');
-    newDivStudent.setAttribute('class','div-content')
-    newDivName.setAttribute('class','div-name');
-    newDivProgress.setAttribute('class','div-progress');
-    divPrincipal.setAttribute('class','col-md-4');
+    newDivStudent.setAttribute('id', 'div-student');
+    newDivStudent.setAttribute('class', 'div-content');
+    newDivName.setAttribute('class', 'div-name');
+    newDivProgress.setAttribute('class', 'div-progress');
+    divPrincipal.setAttribute('class', 'col-md-4');
 
     //creando etiqueta p y li
     const etiquetaName = document.createElement('p');
@@ -119,7 +115,7 @@ function dataTable(datos) {
     //creando contenido
     const newContentName = document.createTextNode(datos[i].name);
     const newContentPercentProgress = document.createTextNode('Progreso General: ' + datos[i].stats.percent + '%');
-    const newContentPercentReads = document.createTextNode('% Lecturas: ' + datos[i].stats.reads.percent + '%'); 
+    const newContentPercentReads = document.createTextNode('% Lecturas: ' + datos[i].stats.reads.percent + '%');
     const newContentPercentExercises = document.createTextNode('% Ejercicios: ' + datos[i].stats.exercises.percent + '%');
     const newContentPercentQuizzes = document.createTextNode('% Quizzes: ' + datos[i].stats.quizzes.percent + '%');
     //añadiendo contenido a cada item de la lista
@@ -135,66 +131,16 @@ function dataTable(datos) {
     //añadiendo lista a un div
     newDivProgress.appendChild(newList);
 
-    etiquetaName.appendChild(newContentName); 
+    etiquetaName.appendChild(newContentName);
     newDivName.appendChild(etiquetaName);
-    
+
     newDivStudent.appendChild(newDivName);
     newDivStudent.appendChild(newDivProgress);
 
     divPrincipal.appendChild(newDivStudent);
     sectionContentStudents.appendChild(divPrincipal);
-    
-    // document.body.insertBefore(newDiv, currentDiv);
-    /* let tr = document.createElement('tr');
-    let celdaName = document.createElement('td');
-    let celdaProgressPercent = document.createElement('td');
-    let celdaExercisesPercent = document.createElement('td');
-    let celdaReadsPercent = document.createElement('td');
-    let celdaQuizzesPercent = document.createElement('td');
-    let textName = document.createTextNode(datos[i].name);
-    const textProgress = document.createTextNode(datos[i].stats.percent + '%')
-    let textExercises = document.createTextNode(datos[i].stats.exercises.percent + '%');
-    const textReadsPercent = document.createTextNode(datos[i].stats.reads.percent + '%')
-    const textQuizzesPercent = document.createTextNode(datos[i].stats.quizzes.percent + '%')
-    celdaName.appendChild(textName);
-    celdaProgressPercent.appendChild(textProgress);
-    celdaExercisesPercent.appendChild(textExercises);
-    celdaReadsPercent.appendChild(textReadsPercent);
-    celdaQuizzesPercent.appendChild(textQuizzesPercent);
-    tr.appendChild(celdaName);
-    tr.appendChild(celdaProgressPercent);
-    tr.appendChild(celdaExercisesPercent);
-    tr.appendChild(celdaQuizzesPercent);
-    tr.appendChild(celdaReadsPercent);
-    tblBody.appendChild(tr); */
   }
 }
-
-/* function addCohorts() {
-  const dataCohorts = JSON.parse(event.target.responseText);
-
-  for (i in dataCohorts) {
-    let option = document.createElement('option');
-    option.setAttribute('value', dataCohorts[i].id)
-    option.innerText += dataCohorts[i].id;
-    generacion.appendChild(option);
-  }
-  generacion.addEventListener('change', function (e) {
-    //console.log(generacion.value)
-    if (generacion.value === 'lim-2018-03-pre-core-pw') {
-
-      getUsers();
-    } else {
-      tblBody.innerHTML = 'no hay datos para mostrar';
-      getUsers();
-    }
-  });
-}
-selectSede.addEventListener('click', function (e) {
-  e.preventDefault();
-  generacion.innerHTML = '';
-  getCohorts(addCohorts);
-}); */
 
 function filterSelect() {
   getCohorts((e) => {
@@ -202,7 +148,7 @@ function filterSelect() {
 
     const filterItems = query => {
       return dataCohorts.filter(sede => {
-        return sede.id.toLowerCase().indexOf(query.toLowerCase()) > -1
+        return sede.id.toLowerCase().indexOf(query.toLowerCase()) > -1;
       });
     }
 
@@ -210,33 +156,32 @@ function filterSelect() {
     sedeSelect.addEventListener('change', function (e) {
       e.preventDefault();
       let index = sedeSelect.value;
-      const dataFilter = filterItems(index);      
-      let s = query => {
+      const dataFilter = filterItems(index);
+      let filterSede = query => {
         return dataFilter.filter(programa => {
-          return programa.id.toLowerCase().indexOf(query.toLowerCase()) > -1
+          return programa.id.toLowerCase().indexOf(query.toLowerCase()) > -1;
         });
       }
-      
+
       selectProgram.addEventListener('change', function (e) {
-      // generacion.innerHTML = ''
+        // generacion.innerHTML = ''
         document.innerHTML = generacion.value = '0'; // fijar opción de selección
         document.innerHTML = generacion.length = 1; // elimina la opción anterior seleccionada
         let valueProgram = selectProgram.value;
-
-        let endFilter = s(valueProgram);
-        console.log(endFilter);
-        
+        let endFilter = filterSede(valueProgram);
         for (i in endFilter) {
           let option = document.createElement('option');
           option.setAttribute('value', endFilter[i].id)
           option.innerText = endFilter[i].id;
-          generacion.appendChild(option);
+          generationSelect.appendChild(option);
         }
       });
-      generacion.addEventListener('change', function (e) {
-        if (generacion.value === 'lim-2018-03-pre-core-pw') {
+      generationSelect.addEventListener('change', function (e) {
+        if (generationSelect.value === 'lim-2018-03-pre-core-pw') {
           getUsers();
-        } 
+        } else {
+          sectionContentStudents.innerHTML = '';
+        }
       });
     });
   });
